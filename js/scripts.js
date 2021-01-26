ad9a6368f9cb387f9481f7e14be1907695f0e7fa
LuisaTheAmaral <luisa.amaral9@gmail.com>
2021-01-26 16:22:15 +0000

    github button added and folders organized

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+// TYPING EFFECT      
+var i = 0;
+var txt = 'welcome!';
+var speed = 80;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("msg").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+function displayUser() {
+    var x = document.getElementById("user-main-screen"); 
+    x.style.display = "block";
+}
+
+function displayEducation() {
+    var x = document.getElementById("education-main-screen"); 
+    x.style.display = "block";
+}
+
+function hideUser() {
+    var x = document.getElementById("user-main-screen"); 
+    x.style.display = "none";
+}
+
+function hideEducation() {
+    var x = document.getElementById("education-main-screen"); 
+    x.style.display = "none";
+}
+
+function hideTerminal() {
+    var x = document.getElementById("terminal-main-screen"); 
+    x.style.display = "none";
+}
+
+// function display() {
+//     var x = document.createElement("input");
+//     x.setAttribute("type", "text");
+//     document.body.appendChild(x);
+//     document.getElementById("terminal__prompt--cursor").appendChild(x);
+    
+    
+//     var y = document.getElementById("cmd").style.backgroundColor = "red"; 
+//     document.getElementById("user-main-screen").style.display = "absolute";
+// }
